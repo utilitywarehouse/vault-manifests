2b20be17ca16577ae124ac94476c8863dc783a30
Ashok Siyani <ashok.siyani@gmail.com>
2025-02-11 14:56:47 +0000

    update script to bootstrap PKI

---
+++ vault-toolkit/vault-pki-initializer.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# This script generate the CA certificate+key and server certificate+key on a
+# start only if there is no existing cert or its expired
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+: "${VAULT_NAMESPACE:?Need to set VAULT_NAMESPACE}"
+
+secret_name="${VAULT_SECRET_NAME:-"vault-tls"}"
+vault_name="${VAULT_NAME:-"vault"}"
+replicas="${VAULT_REPLICAS:-"3"}"
+ca_crt="${VAULT_CACERT:-"/etc/tls/ca.crt"}"
+
+output_dir="./pki"
+rm -rf "${output_dir}"
+mkdir "${output_dir}"
+
+# Exit if certificate exits and its not expiring soon
+if [[ -f "${ca_crt}" ]]; then
+    now_seconds=$(date +%s)
+    cert_expiration=$(cfssl certinfo -cert "${ca_crt}" | jq -r '.not_after' | sed -e 's/T/ /g' -e 's/Z$//g')
+    expiration_seconds=$(date -d "${cert_expiration}" +%s)
+    validity=$((expiration_seconds - now_seconds))
+    if [[ ${validity} -gt 300 ]]; then # 5m
+        echo "certificate found with validity:${validity}s exiting"
+        exit 0
+    fi
+fi
+
+echo "Rotating PKI"
+# CA files
+ca_config='{
+"CN": "'"${VAULT_NAMESPACE}"' CA",
+"key": {
+    "algo": "ecdsa",
+    "size": 521
+},
+"ca": {
+    "expiry": "25h"
+}
+}'
+
+echo "${ca_config}" > "${output_dir}"/ca-config.json
+
+cfssl gencert \
+-initca "${output_dir}"/ca-config.json | cfssljson -bare "${output_dir}"/ca
+
+mv "${output_dir}"/ca.pem "${output_dir}"/ca-cert.pem
+rm "${output_dir}"/ca.csr
+
+# Vault server files
+server_config='{
+"CN": "'"${vault_name}"'.'"${VAULT_NAMESPACE}"'",
+"key": {
+    "algo": "ecdsa",
+    "size": 521
+}
+}'
+hosts='
+    "localhost",
+    "127.0.0.1",
+    "'"${vault_name}"'",
+    "'"${vault_name}-cluster"'",
+    "'"${vault_name}.${VAULT_NAMESPACE}"'",
+    "'"${vault_name}-cluster.${VAULT_NAMESPACE}"'"
+'
+for i in $(seq 0 $((replicas - 1))) ; do
+    hosts='
+        '"${hosts}"',
+        "'"${vault_name}-${i}.${vault_name}"'",
+        "'"${vault_name}-${i}.${vault_name}-cluster"'",
+        "'"${vault_name}-${i}.${vault_name}.${VAULT_NAMESPACE}"'",
+        "'"${vault_name}-${i}.${vault_name}-cluster.${VAULT_NAMESPACE}"'"
+    '
+done
+server_config=$(echo "${server_config}" | jq '.hosts = ['"${hosts}"']')
+
+echo "${server_config}" > "${output_dir}"/server-config.json
+
+cfssl gencert \
+    -ca "${output_dir}"/ca-cert.pem \
+    -ca-key "${output_dir}"/ca-key.pem \
+    "${output_dir}"/server-config.json | cfssljson -bare "${output_dir}"/server
+
+mv "${output_dir}"/server.pem "${output_dir}"/server-cert.pem
+rm "${output_dir}"/server.csr
+
+# Rename files and delete CA key
+mv "${output_dir}"/ca-cert.pem "${output_dir}"/ca.crt
+rm "${output_dir}"/ca-key.pem
+
+mv "${output_dir}"/server-cert.pem "${output_dir}"/tls.crt
+mv "${output_dir}"/server-key.pem "${output_dir}"/tls.key
+
+# Update secrets
+echo "Updating secret in ${VAULT_NAMESPACE}"
+kubectl -n "${VAULT_NAMESPACE}" create secret \
+    generic "${secret_name}" \
+    --from-file "${output_dir}"/ca.crt \
+    --from-file "${output_dir}"/tls.crt \
+    --from-file "${output_dir}"/tls.key \
+    --dry-run=client -o yaml | kubectl -n "${VAULT_NAMESPACE}" replace -f -
+exit_code=$?
+if [ "${exit_code}" != "0" ]; then
+    echo "Error: failed to update secret in ${VAULT_NAMESPACE}, exiting"
+    exit 1
+fi
+
+echo "Rotated successfully on $(date -I'seconds')"

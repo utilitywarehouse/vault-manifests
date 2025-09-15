046ac2ee5eec0e6d4449f930ab0e362e5f7bb32c
Ashok Siyani <ashok.siyani@gmail.com>
2025-09-15 09:32:40 +0100

    add vault token to register plugin

--- vault-toolkit/vault-install-plugin.sh
+++ vault-toolkit/vault-install-plugin.sh
@@ -16,14 +16,27 @@ until curl -Ss -f --cacert "${VAULT_CACERT}" "${local_addr}/v1/sys/init" | jq -e
   sleep 3
 done
 
+if curl -Ss -f --cacert "${VAULT_CACERT}" "${local_addr}/v1/sys/init" | jq -e '.initialized == false' >/dev/null 2>&1; then
+  echo "vault is not initialized, going to sleep";
+  while true; do sleep 86400; done
+fi
+
 # move plugin binary to plugin directory 
 mv /usr/local/bin/vault-plugin-secrets-github /vault/plugins/vault-plugin-secrets-github
 
-sleep inf
+
+# VAULT_TOKEN is required to register plugin binary
+until [ -n "$VAULT_TOKEN" ]; do
+    echo "VAULT_TOKEN to be set, going to sleep"
+    sleep 3
+done
 
 # add plugin to the catalog
-curl -Ss -f --cacert "${VAULT_CACERT}" "${local_addr}v1/sys/plugins/catalog/secret/github" \
+# SECRETS_GH_PLUGIN_VERSION and SECRETS_GH_PLUGIN_SHA env value comes from image 
+# which is added at build time 
+curl -Ss -f --cacert "${VAULT_CACERT}" "${local_addr}/v1/sys/plugins/catalog/secret/github" \
   --request POST                            \
+  --header "X-Vault-Token: ${VAULT_TOKEN}"  \
   --data '{
     "command": "vault-plugin-secrets-github",
     "sha256": "'"${SECRETS_GH_PLUGIN_SHA}"'",
